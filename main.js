db85a4802a8adbf2e2bd10f2acc4f3fa85118205
Shivank254 <71582134+Shivank254@users.noreply.github.com>
2020-11-18 18:56:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,4 +15,33 @@ recognition.onresult = function run (event){
     console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
+
+    speak();
 }
+
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").value;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    console.log(pngqualityinput);
+
+    Webcam.attach(camara)
+}
+
+pngqualityinput = document.getElementById("pngquality").value;
+
+Webcam.set({
+    width:360,
+    height:360,
+    image_format:'jpeg',
+    jpeg_quality:pngqualityinput
+});
+
+camara = document.getElementById("camera")
